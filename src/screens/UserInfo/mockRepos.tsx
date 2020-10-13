96c5e98b639fe9d8309a4707e1d813f5b6b98c17
rssviana <ronanssviana@gmail.com>
2020-10-12 23:38:17 -0300

    feat(#5): :art: Adding UserInfo Page

---
+++ src/screens/UserInfo/mockRepos.tsx
@@ -0,0 +1,26 @@
+export const repos = [
+  {
+    title: 'Game of thrones',
+    link: 'https://google.com',
+    description: 'One of the best tv series of the world',
+    stars: '5',
+  },
+  {
+    title: 'Flea Bag',
+    link: 'https://google.com',
+    description: 'One of the best tv series of the world',
+    stars: '5',
+  },
+  {
+    title: 'The Walking Dead',
+    link: 'https://google.com',
+    description: 'One of the best tv series of the world',
+    stars: '4.5',
+  },
+  {
+    title: 'Vikings',
+    link: 'https://google.com',
+    description: 'One of the best tv series of the world',
+    stars: '4',
+  },
+];
