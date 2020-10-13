69f124687093ba97c6ac38a24c6eab3a921aafbb
rssviana <ronanssviana@gmail.com>
2020-10-12 21:33:51 -0300

    refactor(#1): :wastebasket: Removing unnecessary files from porject

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/rssviana/foribus-oraculi"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to repository
         </a>
       </header>
     </div>
